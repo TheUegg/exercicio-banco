367cde3ee2b186617e9e18da9627d9e80978fa59
José Marcos <48721602+FrenzyxD@users.noreply.github.com>
2019-03-19 15:42:47 -0300

    Mudadinha de leve
    Adicionei alguns métodos pra acessar os atributos, arrumei os atributos, ajeitei o construtor... Não fiz o método pra mudar o numero da conta porque creio que ninguém muda numero de conta ..

--- Conta.java
+++ Conta.java
@@ -1,13 +1,15 @@
 
 public class Conta {
 	
-	int limiteSaque;
+	protected int limiteSaque;
+	protected int numero;
+	protected float saldo;
+	protected Movimentacao[] movimentacao = new Movimentacao[20];
 	
-	public Conta() {
-	}
-	
-	public Conta(int limiteSaque) {
+	public Conta() (int limiteSaque,int numero, float saldo){
 		this.limiteSaque = limiteSaque;
+		this.numero = numero;
+		this.saldo = saldo;
 	}
 
 	public int getLimiteSaque() {
@@ -17,6 +19,20 @@ public class Conta {
 	public void setLimiteSaque(int limiteSaque) {
 		this.limiteSaque = limiteSaque;
 	}
+	
+	public int getNumero(){
+		return this.numero;
+	}
+	
+	public float getSaldo(){
+		return this.saldo;
+		
+	}
+	
+	public void setSaldo( float saldo){
+		this.saldo= saldo;
+	}
+	
 
 	
 }
